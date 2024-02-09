6fa09b79d946198a2c5f8c0d3e412f813a114aaa
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-09 09:53:10 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e
-version=$(curl -fsSL "https://duplicacy.com/latest_web_version" | jq -re .latest)
+version=$(curl -fsSL "https://duplicacy.com/latest_web_version" | jq -re .latest) || exit 1
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
