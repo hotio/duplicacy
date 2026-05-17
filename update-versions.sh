e92181c447d95d0010bd50c260dc224a57961f16
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-05-17 16:24:01 +0200

    add __command [skip ci]

--- update-versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exuo pipefail
-
-version=$(curl -fsSL "https://duplicacy.com/latest_web_version" | jq -re .latest)
-json=$(cat meta.json)
-jq --sort-keys \
-    --arg version "${version//v/}" \
-    '.version = $version' <<< "${json}" | tee meta.json
